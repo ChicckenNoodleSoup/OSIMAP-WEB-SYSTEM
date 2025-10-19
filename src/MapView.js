7d294aa9061fb6342e0c2d6aa590c803d53d920b
Simon Reyes <reyesimon31@gmail.com>
2025-10-19 14:35:52 +0800

    added spinner for viewMap and Print

--- src/MapView.js
+++ src/MapView.js
@@ -631,9 +631,23 @@ export default function MapView() {
   if (loading) return (
     <div className="scroll-wrapper">
       <div className="mapview-container">
-        <div className="page-header">
-          <h6 className="page-title">Loading Clustered Data...</h6>
-          <DateTime />
+        <div className="loading-spinner-container">
+          <svg 
+            className="loading-spinner" 
+            viewBox="-13 -13 45 45" 
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle className="box5631" cx="13" cy="1" r="5"/>
+            <circle className="box5631" cx="25" cy="1" r="5"/>
+            <circle className="box5631" cx="1" cy="13" r="5"/>
+            <circle className="box5631" cx="13" cy="13" r="5"/>
+            <circle className="box5631" cx="25" cy="13" r="5"/>
+            <circle className="box5631" cx="1" cy="25" r="5"/>
+            <circle className="box5631" cx="13" cy="25" r="5"/>
+            <circle className="box5631" cx="25" cy="25" r="5"/>
+            <circle className="box5631" cx="1" cy="1" r="5"/>
+          </svg>
+          <p className="loading-text">Loading Clustered Data...</p>
         </div>
       </div>
     </div>
