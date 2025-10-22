128130a8b30f15cb79225f045119e55608d4bbe1
Snow-15 <github.dimple756@passinbox.com>
2025-10-22 12:38:19 +0800

    inayos yung headers para consistent yung position

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from './UserContext'; // Only new import added
 import './Dashboard.css';
+import './PageHeader.css';
 import { DateTime } from './DateTime';
 import { createClient } from '@supabase/supabase-js';
 
